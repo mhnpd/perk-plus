80e4a25ea5d5e4a9124933f8f43b6e072eaaa87a
mhnpd <mhnpd@outlook.com>
2024-07-24 21:02:46 +0200

    refactor: Update main.tsx to use BrowserRouter for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import ThemeProvider from './theme/theme-provider'
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')
 const root = createRoot(container as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-     <h1>Hello, world!</h1>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <h1>Hello, world!</h1>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)

040a6c45cd6e0c789c7c8f63bf854e6cde0c0c5a
mhnpd <mhnpd@outlook.com>
2024-07-25 18:24:31 +0200

    refactor: Remove unnecessary line in AuthGuard component

--- src/shared/axios-config.ts
+++ src/shared/axios-config.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { getAuthToken, removeAuthToken } from '../api/user-login'
+import { getAuthToken } from '../api/user-login'
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_UPSTREAM_API_URL,
@@ -16,10 +16,6 @@ axiosInstance.interceptors.response.use(
     return response
   },
   (error) => {
-    // If the response status is 401, log out the user
-    if (error.response && error.response.status === 401) {
-      removeAuthToken()
-    }
     // Otherwise, reject the promise with the error object
     return Promise.reject(error)
   }

9a314dd091805e873ee472aed4d3d42dfd87c2f1
mhnpd <mhnpd@outlook.com>
2024-07-27 12:41:49 +0200

    refactor: Update home page layout and add lazy loading for images

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,10 @@ import { AppConfig } from '../../constants/config'
 import Container from '@mui/material/Container'
 import LoyaltyCard from './loyalty-card'
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import Stack from '@mui/material/Stack'
+import Card from '@mui/material/Card'
 
 export default function Home() {
 
@@ -11,16 +15,35 @@ export default function Home() {
       <Helmet>
         <title>{`Home | ${AppConfig.AppName}`}</title>
       </Helmet>
-      <Container fixed>
-        <Box
+      <Container maxWidth="xl">
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Hi, Welcome back 👋
+        </Typography>
+        <Card
+          component={Stack}
+          spacing={3}
+          direction="row"
           sx={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(2, 1fr)',
-            gap: 10,
+            px: 3,
+            py: 5,
+            borderRadius: 2,
           }}
         >
-          <LoyaltyCard />
-        </Box>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={8}>
+              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <img
+                  src="https://source.unsplash.com/animal"
+                  alt="random"
+                  style={{ width: '100%', height: '100%' }}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <LoyaltyCard />
+            </Grid>
+          </Grid>
+        </Card>
       </Container>
     </>
   )
